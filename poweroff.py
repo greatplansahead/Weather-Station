1e6dcf2386fcc487196676b49f1ad75d28062acc
greatplansahead <117952204+greatplansahead@users.noreply.github.com>
2024-10-29 09:47:40 -0600

    Update poweroff.py
    to work with digitalio library

--- poweroff.py
+++ poweroff.py
@@ -1,23 +1,27 @@
 # This is a a code to poweroff the Raspberry Pi when pressing and holding a defined button
 
 # External module imports
-import RPi.GPIO as GPIO
+import digitalio
+import board
 import time
 import os
 
 # Raspberry Pi pin & variables definitons &
 hold_time = 3                   # Hold time in sec to poweroff
-button_poweroff = 1             # Push button to turn off Raspberry Pi
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
-GPIO.setup(button_poweroff, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Button set as input
+button_poweroff_pin = board.D1 
+
+#setups up the button
+button_poweroff = digitalio.DigitalInOut(button_poweroff_pin)
+button_poweroff.direction = digitalio.Direction.INPUT
+button_poweroff.pull = digitalio.Pull.DOWN
+
 
 while True:
-    GPIO.wait_for_edge(button_poweroff, GPIO.RISING)
+   while not button_poweroff.value:
     start = time.time()
     time.sleep(0.2)     # Switch debounce
 
-    while GPIO.input(button_poweroff) == 1:
+    while button_poweroff.value:
         time.sleep(0.01)
     length = time.time() - start
 
